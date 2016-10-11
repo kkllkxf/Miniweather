651c1f178a377f2df4330110705f6225b1096f0c
unknown <>
2016-10-11 22:27:00 +0800

    first commit

---
+++ app/src/main/java/kangxiaofei/miniweather/MainActivity.java
@@ -0,0 +1,15 @@
+package kangxiaofei.miniweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState) ;
+        setContentView(R.layout.weather_info);
+    }
+}
