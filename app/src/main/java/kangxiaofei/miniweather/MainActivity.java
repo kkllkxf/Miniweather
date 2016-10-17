a6f7aa25b5eb8f6bf426f88216641fcfa31840f2
unknown <>
2016-10-17 22:37:31 +0800

    网络状态检查！

--- app/src/main/java/kangxiaofei/miniweather/MainActivity.java
+++ app/src/main/java/kangxiaofei/miniweather/MainActivity.java
@@ -2,14 +2,27 @@ package kangxiaofei.miniweather;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.util.Log;
 
+import cn.edu.pku.kangxiaofei.util.NetUtil;
 /**
  * Created by Administrator on 2016/10/11.
  */
 public class MainActivity extends Activity {
     @Override
     protected void onCreate( Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState) ;
+        super.onCreate(savedInstanceState) ;
         setContentView(R.layout.weather_info);
+
+        if ( NetUtil. getNetworkState( this) != NetUtil. NETWORN_NONE) {
+            Log.d( "myWeather", "网络OK") ;
+            Toast.makeText( MainActivity. this, "网络OK！", Toast.LENGTH_LONG).show( ) ;
+        }
+        else
+        {
+            Log.d( "myWeather", " 网络挂了 ") ;
+            Toast.makeText( MainActivity. this, " 网络挂了！", Toast.LENGTH_LONG).show( ) ;
+        }
     }
 }
